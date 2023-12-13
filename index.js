79ddd74dd24302c5511120c4713fed5e3d9e4315
Alexander E. Dang <alex.dang3606@gmail.com>
2023-12-14 00:18:48 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json({limit: '50mb'}));       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', parameterLimit: 100000, extended: true}));
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
